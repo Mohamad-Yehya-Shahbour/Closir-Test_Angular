cf9d809e485f7b02b20103cfe28204de650dbca0
Mohamad Yehya <mohamad.yehya.shahbur@gmail.com>
2022-08-29 01:31:13 +0300

    add todos dummydata

--- src/app/providers/todos.states.ts
+++ src/app/providers/todos.states.ts
@@ -2,3 +2,18 @@ export interface TodoModel {
     id : number;
     title : string;
 }
+
+export let todos:TodoModel[] = [
+    {
+        id : 1,
+        title : 'medidate 1 hour'
+    },
+    {
+        id : 2,
+        title : 'sport 30 minutes'
+    },
+    {
+        id : 3,
+        title : 'read a book'
+    }
+]
