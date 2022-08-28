46db7a7ef56646baa2f5383957c57e7ab7ec5299
Mohamad Yehya <mohamad.yehya.shahbur@gmail.com>
2022-08-29 01:35:29 +0300

    handle input and add additem function

--- src/app/components/todo/todo-input/todo-input.component.ts
+++ src/app/components/todo/todo-input/todo-input.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { todos } from 'src/app/providers/todos.states';
 
 @Component({
   selector: 'app-todo-input',
@@ -6,10 +7,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./todo-input.component.css']
 })
 export class TodoInputComponent implements OnInit {
-
+  inputValue :any;
+  todoLength:any;
   constructor() { }
 
   ngOnInit(): void {
+    
+  }
+  getInputValue(value:string){
+    this.inputValue = value;
+    //console.warn(value)
+  }
+  addItem(){
+    this.todoLength = todos.length
+    todos.push({
+      id: this.todoLength+1,
+      title: this.inputValue
+    })
+    this.inputValue=""
   }
 
 }
