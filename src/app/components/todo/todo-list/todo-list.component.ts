e9dd3bb7c2da342d7cd5642c9ea78e41d8095476
Mohamad Yehya <mohamad.yehya.shahbur@gmail.com>
2022-08-29 01:32:06 +0300

    fetch todos

--- src/app/components/todo/todo-list/todo-list.component.ts
+++ src/app/components/todo/todo-list/todo-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { todos } from 'src/app/providers/todos.states';
 
 @Component({
   selector: 'app-todo-list',
@@ -6,10 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./todo-list.component.css']
 })
 export class TodoListComponent implements OnInit {
-
+  todos :any;
   constructor() { }
 
   ngOnInit(): void {
+    this.todos = todos
   }
 
 }
