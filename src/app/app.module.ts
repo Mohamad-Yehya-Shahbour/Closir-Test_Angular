53ab302eb7e9b775c500daeae2e166edb279a0ef
Mohamad Yehya <mohamad.yehya.shahbur@gmail.com>
2022-08-28 23:40:10 +0300

    add components to app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { TodoComponent } from './components/todo/todo.component';
+import { ChartComponent } from './components/chart/chart.component';
+import { AboutComponent } from './components/about/about.component';
+import { TodoInputComponent } from './components/todo/todo-input/todo-input.component';
+import { TodoListComponent } from './components/todo/todo-list/todo-list.component';
+import { TodoItemComponent } from './components/todo/todo-item/todo-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoComponent,
+    ContactComponent,
+    ChartComponent,
+    AboutComponent,
+    TodoInputComponent,
+    TodoListComponent,
+    TodoItemComponent
   ],
   imports: [
     BrowserModule,
