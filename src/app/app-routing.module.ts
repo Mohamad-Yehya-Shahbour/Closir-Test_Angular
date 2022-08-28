a0291a5610b0328ce863280a99c5dff823d47a74
Mohamad Yehya <mohamad.yehya.shahbur@gmail.com>
2022-08-28 23:34:06 +0300

    update routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './components/about/about.component';
+import { ChartComponent } from './components/chart/chart.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { TodoComponent } from './components/todo/todo.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    { path: '', component: TodoComponent },
+  	{ path: 'contact', component: ContactComponent },
+    { path: 'about', component: AboutComponent },
+    { path: 'chart', component: ChartComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+
+ }
