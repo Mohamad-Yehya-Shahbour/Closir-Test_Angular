cb1a447326a9f091905fa9ab724037749ce92458
Mohamad Yehya <mohamad.yehya.shahbur@gmail.com>
2022-08-30 20:59:24 +0300

    structuring charts design

--- src/app/components/chart/chart.component.ts
+++ src/app/components/chart/chart.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, NgZone, OnInit } from '@angular/core';
+import * as am4core from "@amcharts/amcharts4/core";
+import * as am4charts from "@amcharts/amcharts4/charts";
+import am4themes_animated from "@amcharts/amcharts4/themes/animated"
+
+am4core.useTheme(am4themes_animated);
 
 @Component({
   selector: 'app-chart',
   templateUrl: './chart.component.html',
   styleUrls: ['./chart.component.css']
 })
-export class ChartComponent implements OnInit {
+export class ChartComponent implements OnInit{
 
   constructor() { }
+  
 
   ngOnInit(): void {
   }
+
+
+
 
 }
