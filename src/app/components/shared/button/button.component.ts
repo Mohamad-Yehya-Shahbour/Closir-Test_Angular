f5e88e7338a492b59e20912d23b8347e4a6b2723
Mohamad Yehya <mohamad.yehya.shahbur@gmail.com>
2022-08-30 22:36:48 +0300

    add onClick handler and click function

--- src/app/components/shared/button/button.component.ts
+++ src/app/components/shared/button/button.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-button',
@@ -6,10 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./button.component.css']
 })
 export class ButtonComponent implements OnInit {
+  @Input() message :string;
+  @Input() data:any
+  @Output() onClick = new EventEmitter<any>();
 
+  
   constructor() { }
 
   ngOnInit(): void {
+    
+  }
+
+  onClickbutton() {
+    this.onClick.emit();
   }
 
 }
