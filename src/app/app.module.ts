ffb93d03917daae77ad42b7368e93bfbb3d36055
Mohamad Yehya <mohamad.yehya.shahbur@gmail.com>
2022-08-31 22:47:11 +0300

    update paths after moving body components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ContactComponent } from './components/contact/contact.component';
-import { TodoComponent } from './components/todo/todo.component';
-import { ChartComponent } from './components/chart/chart.component';
-import { AboutComponent } from './components/about/about.component';
-import { TodoInputComponent } from './components/todo/todo-input/todo-input.component';
-import { TodoListComponent } from './components/todo/todo-list/todo-list.component';
-import { TodoItemComponent } from './components/todo/todo-item/todo-item.component';
-import { XyChartComponent } from './components/chart/xy-chart/xy-chart.component';
-import { PieChartComponent } from './components/chart/pie-chart/pie-chart.component';
+import { ContactComponent } from './components/body/contact/contact.component';
+import { TodoComponent } from './components/body/todo/todo.component';
+import { ChartComponent } from './components/body/chart/chart.component';
+import { AboutComponent } from './components/body/about/about.component';
+import { TodoInputComponent } from './components/body/todo/todo-input/todo-input.component';
+import { TodoListComponent } from './components/body/todo/todo-list/todo-list.component';
+import { TodoItemComponent } from './components/body/todo/todo-item/todo-item.component';
+import { XyChartComponent } from './components/body/chart/xy-chart/xy-chart.component';
+import { PieChartComponent } from './components/body/chart/pie-chart/pie-chart.component';
 import { ButtonComponent } from './components/shared/button/button.component';
-import { GaugeChartComponent } from './components/chart/gauge-chart/gauge-chart.component';
+import { GaugeChartComponent } from './components/body/chart/gauge-chart/gauge-chart.component';
 import { AlertComponent } from './components/shared/alert/alert.component';
+import { TopBarComponent } from './components/navigations/top-bar/top-bar.component';
+import { SideBarComponent } from './components/navigations/side-bar/side-bar.component';
 
 
 @NgModule({
@@ -32,6 +34,8 @@ import { AlertComponent } from './components/shared/alert/alert.component';
     ButtonComponent,
     GaugeChartComponent,
     AlertComponent,
+    TopBarComponent,
+    SideBarComponent
   ],
   imports: [
     BrowserModule,
