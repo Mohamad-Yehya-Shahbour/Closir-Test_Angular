5b8e5d62d3d8a5c5932fdc42198b594965229f7e
Mohamad Yehya <mohamad.yehya.shahbur@gmail.com>
2022-08-31 22:45:24 +0300

    update components paths

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AboutComponent } from './components/about/about.component';
-import { ChartComponent } from './components/chart/chart.component';
-import { ContactComponent } from './components/contact/contact.component';
-import { TodoComponent } from './components/todo/todo.component';
+import { AboutComponent } from './components/body/about/about.component';
+import { ChartComponent } from './components/body/chart/chart.component';
+import { ContactComponent } from './components/body/contact/contact.component';
+import { TodoComponent } from './components/body/todo/todo.component';
 
 const routes: Routes = [
     { path: '', component: TodoComponent },
